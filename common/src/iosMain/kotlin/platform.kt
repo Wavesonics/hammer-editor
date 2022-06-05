941e1c8e79a57aec17d0dae5b300d3e977538a86
Wavesonics <adamwbrown@gmail.com>
2022-06-05 13:22:53 -0700

    Enabled HMPP This fixes ios platform calls

--- common/src/iosMain/kotlin/platform.kt
+++ common/src/iosMain/kotlin/platform.kt
@@ -1,12 +1,14 @@
 package com.darkrockstudios.apps.hammer.common
 
 import com.darkrockstudios.apps.hammer.common.data.Project
+import platform.Foundation.NSLog
 
 actual fun getPlatformName(): String {
     return "iOS"
 }
 
 actual fun getProjectsForDirectory(path: String): List<Project> {
+    NSLog("Test")
     return listOf(
         Project(name = "Proj 1", path="/a/b/c"),
         Project(name = "Proj 2", path="/a/b/c"),

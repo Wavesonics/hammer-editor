f8f5be6ade860c48114725f50addccee7f64147c
Adam Brown <adamwbrown@gmail.com>
2022-06-14 18:29:17 -0700

    Na

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi

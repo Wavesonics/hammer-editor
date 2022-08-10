cf0d9c2afb15ef416858bfc0dd3d0a5de8bb9418
Adam Brown <adamwbrown@gmail.com>
2022-08-09 22:50:24 -0700

    Collapsing groups works dragging is about half working

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecteditor/scenelist/SceneListUi.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecteditor/scenelist/SceneListUi.kt
@@ -1,23 +1,21 @@
 package com.darkrockstudios.apps.hammer.common.projecteditor.scenelist
 
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 import com.darkrockstudios.apps.hammer.common.compose.Ui
 import com.darkrockstudios.apps.hammer.common.data.SceneItem
+import com.darkrockstudios.apps.hammer.common.data.SceneSummary
 import com.darkrockstudios.apps.hammer.common.tree.TreeValue
 import org.burnoutcrew.reorderable.*
 
@@ -33,17 +31,8 @@ fun SceneListUi(
 
     val summary = state.scenes
 
-    val reorderState = rememberReorderableLazyListState(
-        onMove = { from, to ->
-            if (summary != null) {
-                component.updateSceneOrder(from = from.index, to = to.index)
-            }
-        },
-        onDragEnd = { from, to ->
-            // TODO movescene
-            //component.moveScene(from = from, to = to)
-        }
-    )
+    //component.updateSceneOrder(from = from.index, to = to.index)
+    //component.moveScene(from = from, to = to)
 
     Column(modifier = modifier.fillMaxWidth().padding(Ui.PADDING)) {
         TextField(
@@ -73,38 +62,24 @@ fun SceneListUi(
             )
         }
 
-        /*
-        SceneTree(
-            summary = summary,
-            modifier = Modifier.fillMaxHeight().fillMaxWidth()
-        )
-        */
-
-        LazyColumn(
-            state = reorderState.listState,
-            modifier = Modifier.fillMaxHeight().fillMaxWidth().reorderable(reorderState)
-                .detectReorderAfterLongPress(reorderState),
-            contentPadding = PaddingValues(Ui.PADDING)
-        ) {
-            if (summary != null) {
-                items(
-                    summary.sceneTree.totalChildren,
-                    { summary.sceneTree[it].value.id }) { item ->
-                    val sceneNode = summary.sceneTree[item]
-                    if (!sceneNode.value.isRootScene) {
-                        ReorderableScene(
-                            sceneNode,
-                            reorderState,
-                            state,
-                            component
-                        ) { deleteTarget ->
-                            sceneDefDeleteTarget = deleteTarget
-                        }
-                    }
-                }
+        if (summary != null) {
+            SceneTree(
+                summary = summary,
+                modifier = Modifier.fillMaxHeight().fillMaxWidth()
+            ) { sceneNode, toggleExpand, draggable ->
+                SceneNode(
+                    sceneNode = sceneNode,
+                    draggable = draggable,
+                    state = state,
+                    summary = summary,
+                    component = component,
+                    toggleExpand = toggleExpand,
+                    sceneDefDeleteTarget = { deleteTarget ->
+                        sceneDefDeleteTarget = deleteTarget
+                    },
+                )
             }
         }
-
     }
 
     sceneDefDeleteTarget?.let { scene ->
@@ -118,42 +93,37 @@ fun SceneListUi(
 }
 
 @Composable
-fun ReorderableScene(
+fun SceneNode(
     sceneNode: TreeValue<SceneItem>,
-    reorderState: ReorderableLazyListState,
+    draggable: Modifier,
     state: SceneList.State,
+    summary: SceneSummary,
     component: SceneList,
-    sceneDefDeleteTarget: (SceneItem) -> Unit
+    toggleExpand: () -> Unit,
+    sceneDefDeleteTarget: (SceneItem) -> Unit,
 ) {
-    val summary = state.scenes ?: return
-    val scene: SceneItem = sceneNode.value
-
-    ReorderableItem(reorderState, key = scene.id) { isDragging ->
-        val elevation = animateDpAsState(if (isDragging) 16.dp else 0.dp)
-        Column(
-            modifier = Modifier
-                .shadow(elevation.value)
-                .background(MaterialTheme.colors.surface)
-        ) {
-            val isSelected = scene == state.selectedSceneItem
-            if (scene.type == SceneItem.Type.Scene) {
-                SceneItem(
-                    scene,
-                    sceneNode.depth,
-                    summary.hasDirtyBuffer.contains(scene.id),
-                    isSelected,
-                    component::onSceneSelected
-                ) { selectedScene ->
-                    sceneDefDeleteTarget(selectedScene)
-                }
-            } else {
-                SceneGroupItem(
-                    sceneNode,
-                    summary.hasDirtyBuffer,
-                    component::onSceneSelected
-                )
-            }
-        }
+    val scene = sceneNode.value
+    val isSelected = scene == state.selectedSceneItem
+    if (scene.type == SceneItem.Type.Scene) {
+        SceneItem(
+            scene = scene,
+            draggable = draggable,
+            depth = sceneNode.depth,
+            hasDirtyBuffer = summary.hasDirtyBuffer.contains(scene.id),
+            isSelected = isSelected,
+            onSceneSelected = component::onSceneSelected,
+            onSceneAltClick = { selectedScene ->
+                sceneDefDeleteTarget(selectedScene)
+            },
+        )
+    } else {
+        SceneGroupItem(
+            sceneNode = sceneNode,
+            modifier = draggable,
+            hasDirtyBuffer = summary.hasDirtyBuffer,
+            toggleExpand = toggleExpand,
+            onSceneAltClick = component::onSceneSelected,
+        )
     }
 }
 
@@ -161,100 +131,86 @@ fun ReorderableScene(
 @Composable
 fun SceneItem(
     scene: SceneItem,
+    draggable: Modifier,
     depth: Int,
     hasDirtyBuffer: Boolean,
     isSelected: Boolean,
     onSceneSelected: (SceneItem) -> Unit,
-    onSceneAltClick: (SceneItem) -> Unit
+    onSceneAltClick: (SceneItem) -> Unit,
 ) {
-    Row(
-        modifier = Modifier
+    Card(
+        modifier = draggable
             .fillMaxWidth()
             .padding(
-                start = Ui.PADDING * (depth + 1)
+                start = (Ui.PADDING * depth),
+                top = Ui.PADDING,
+                bottom = Ui.PADDING,
+                end = Ui.PADDING
             )
+            .combinedClickable(
+                onClick = { onSceneSelected(scene) },
+            ),
+        elevation = Ui.ELEVATION,
+        backgroundColor = if (isSelected) selectionColor() else MaterialTheme.colors.surface
     ) {
-        Card(
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
                 .padding(Ui.PADDING)
-                .combinedClickable(
-                    onClick = { onSceneSelected(scene) },
-                    //onLongClick = { onSceneAltClick(scene) }
-                ),
-            elevation = Ui.ELEVATION,
-            backgroundColor = if (isSelected) selectionColor() else MaterialTheme.colors.surface
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            Row(
-                modifier = Modifier
-                    .padding(Ui.PADDING)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-            ) {
+            Text(
+                "Scene: ${scene.name}",
+                style = MaterialTheme.typography.body1
+            )
+            if (hasDirtyBuffer) {
                 Text(
-                    "Scene: ${scene.name}",
-                    style = MaterialTheme.typography.body1
+                    "Unsaved",
+                    style = MaterialTheme.typography.subtitle1
                 )
-                if (hasDirtyBuffer) {
-                    Text(
-                        "Unsaved",
-                        style = MaterialTheme.typography.subtitle1
-                    )
-                }
-                Button({ onSceneAltClick(scene) }) {
-                    Text("X")
-                }
+            }
+            Button({ onSceneAltClick(scene) }) {
+                Text("X")
             }
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SceneGroupItem(
     sceneNode: TreeValue<SceneItem>,
+    modifier: Modifier,
     hasDirtyBuffer: Set<Int>,
-    onSceneAltClick: (SceneItem) -> Unit
+    toggleExpand: () -> Unit,
+    onSceneAltClick: (SceneItem) -> Unit,
 ) {
     val (scene: SceneItem, _, _, children: List<TreeValue<SceneItem>>) = sceneNode
 
-    Row(
-        modifier = Modifier
+    Card(
+        modifier = modifier
             .fillMaxWidth()
-            .padding(
-                start = Ui.PADDING * (sceneNode.depth + 1)
-            )
+            .clickable(onClick = toggleExpand),
+        elevation = Ui.ELEVATION,
+        backgroundColor = MaterialTheme.colors.secondaryVariant
     ) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(Ui.PADDING)
-                .combinedClickable(
-                    onClick = { },
-                    //onLongClick = { onSceneAltClick(scene) }
-                ),
-            elevation = Ui.ELEVATION,
-            backgroundColor = MaterialTheme.colors.secondaryVariant
+        Row(
+            modifier = Modifier.padding(Ui.PADDING).fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Row(
-                modifier = Modifier.padding(Ui.PADDING).fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    "Group: ${scene.name}",
-                    style = MaterialTheme.typography.body1
-                )
+            Text(
+                "Group: ${scene.name}",
+                style = MaterialTheme.typography.body1
+            )
 
-                if (children.any { hasDirtyBuffer.contains(it.value.id) }) {
-                    Text(
-                        "Unsaved",
-                        style = MaterialTheme.typography.subtitle1
-                    )
-                }
-                if (children.isEmpty()) {
-                    Button({ onSceneAltClick(scene) }) {
-                        Text("X")
-                    }
+            if (children.any { hasDirtyBuffer.contains(it.value.id) }) {
+                Text(
+                    "Unsaved",
+                    style = MaterialTheme.typography.subtitle1
+                )
+            }
+            if (children.isEmpty()) {
+                Button({ onSceneAltClick(scene) }) {
+                    Text("X")
                 }
             }
         }
--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecteditor/scenelist/SceneTree.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecteditor/scenelist/SceneTree.kt
@@ -1,28 +1,35 @@
 package com.darkrockstudios.apps.hammer.common.projecteditor.scenelist
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
 import com.darkrockstudios.apps.hammer.common.compose.Ui
 import com.darkrockstudios.apps.hammer.common.data.SceneItem
 import com.darkrockstudios.apps.hammer.common.data.SceneSummary
 import com.darkrockstudios.apps.hammer.common.tree.TreeValue
+import io.github.aakira.napier.Napier
+import kotlin.math.roundToInt
 
 
 @Composable
 fun SceneTree(
     summary: SceneSummary?,
-    modifier: Modifier
+    modifier: Modifier,
+    itemUi: ItemUi
 ) {
     summary ?: return
-    summary.sceneTree
 
-    Canvas(modifier = modifier) {
+    Napier.d("SceneTree")
+    /*
+    Canvas(modifier = modifier.wrapContentHeight()) {
         val canvasWidth = size.width
         val canvasHeight = size.height
 
@@ -32,26 +39,117 @@ fun SceneTree(
             color = Color.Blue
         )
     }
+    */
+    //val interactionSource = remember { MutableInteractionSource() }
 
-    SceneTreeNode(summary.sceneTree.root, Modifier.fillMaxWidth())
+    val scrollState = rememberScrollState()
+
+    var offsetY by remember { mutableStateOf(0f) }
+    var selectedId by remember { mutableStateOf(-1) }
+    val startDragging = { id: Int ->
+        Napier.d("startDragging")
+        if (selectedId == -1) {
+            selectedId = id
+        }
+    }
+    val stopDragging = {
+        Napier.d("stopDragging")
+        selectedId = -1
+        offsetY = 0F
+    }
+
+    var isDragging by remember { mutableStateOf(false) }
+
+    /*
+.draggable(
+    orientation = Orientation.Horizontal,
+    state = rememberDraggableState { delta ->
+        offsetY += delta
+        Napier.d("dragging: $offsetY")
+        if (isDragging) {
+            offsetY += delta
+            Napier.d("is dragging: $offsetY")
+        }
+    }
+)
+*/
+    //scrollState.scrollTo()
+
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState),
+    ) {
+        summary.sceneTree.root.children.forEach { childNode ->
+
+            var draggable = Modifier.pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        startDragging(childNode.value.id)
+                    },
+                    onDragEnd = {
+                        stopDragging()
+                    }
+                ) { _, dragAmount ->
+                    if (selectedId == childNode.value.id) {
+                        offsetY += dragAmount.y
+                    }
+                }
+            }
+
+            if (selectedId == childNode.value.id) {
+                draggable = draggable.offset {
+                    return@offset if (selectedId == childNode.value.id) {
+                        Napier.d("Dragging: $offsetY")
+                        IntOffset(x = 0, y = offsetY.roundToInt())
+                    } else {
+                        IntOffset(0, 0)
+                    }
+                }
+            }
+
+            SceneTreeNode(
+                tree = childNode,
+                dragModifier = draggable,
+                itemUi = itemUi
+            )
+        }
+    }
 }
 
 @Composable
 fun SceneTreeNode(
     tree: TreeValue<SceneItem>,
-    modifier: Modifier
+    dragModifier: Modifier,
+    itemUi: ItemUi
 ) {
+    /*
     Canvas(modifier = modifier) {
         val width = size.width
         val height = size.height
         drawLine(color = Color.Blue, start = Offset(0f, 0f), end = Offset(width, 0f))
     }
-    Text(tree.value.name)
+    */
+    var collapsed by remember { mutableStateOf(false) }
 
-    tree.children.forEach { child ->
-        SceneTreeNode(
-            child,
-            modifier.padding(start = Ui.PADDING)
-        )
+    val toggleExpanded = {
+        collapsed = !collapsed
+    }
+
+    itemUi(tree, toggleExpanded, dragModifier)
+
+    AnimatedVisibility(visible = !collapsed) {
+        tree.children.forEachIndexed { index, child ->
+            SceneTreeNode(
+                tree = child,
+                dragModifier = dragModifier.padding(start = Ui.PADDING * index),
+                itemUi = itemUi,
+            )
+        }
     }
 }
+
+typealias ItemUi = @Composable (
+    node: TreeValue<SceneItem>,
+    toggleExpanded: () -> Unit,
+    draggable: Modifier,
+) -> Unit

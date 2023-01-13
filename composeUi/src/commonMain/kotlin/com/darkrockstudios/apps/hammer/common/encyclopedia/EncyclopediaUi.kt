d9593e0f676f3b2a6deb0263feed5a7b504c147f
Wavesonics <adamwbrown@gmail.com>
2023-01-12 22:08:21 -0800

    Updated EncyclopediaUi some

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/encyclopedia/EncyclopediaUi.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/encyclopedia/EncyclopediaUi.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
@@ -23,13 +26,83 @@ fun EncyclopediaUi(component: Encyclopedia) {
 	val scope = rememberCoroutineScope()
 	val snackbarHostState = remember { SnackbarHostState() }
 
+	var showCreate by remember { mutableStateOf(false) }
+
+	BoxWithConstraints(modifier = Modifier.fillMaxSize().padding(Ui.PADDING)) {
+		if (showCreate) {
+			CreateEntry(
+				component = component,
+				scope = scope,
+				modifier = Modifier.align(Alignment.Center),
+				snackbarHostState = snackbarHostState
+			) {
+				showCreate = false
+			}
+		} else {
+			Column {
+				Text("Encyclopedia")
+
+				Spacer(modifier = Modifier)
+
+				Text("Notes")
+				LazyVerticalGrid(
+					columns = GridCells.Adaptive(512.dp),
+					modifier = Modifier.fillMaxWidth(),
+					contentPadding = PaddingValues(Ui.PADDING)
+				) {
+					if (state.entryDefs.isEmpty()) {
+						item {
+							Text("No Entries Found")
+						}
+					} else {
+						items(state.entryDefs.size) { index ->
+							EntryDefItem(
+								entry = state.entryDefs[index],
+								component = component,
+								snackbarHostState = snackbarHostState,
+								scope = scope
+							)
+						}
+					}
+				}
+			}
+			FloatingActionButton(
+				onClick = { showCreate = true },
+				modifier = Modifier.align(Alignment.BottomEnd)
+			) {
+				Icon(
+					Icons.Rounded.Add,
+					"Create Entry"
+				)
+			}
+		}
+	}
+
+	SnackbarHost(snackbarHostState, modifier = Modifier)
+}
+
+@Composable
+fun CreateEntry(
+	component: Encyclopedia,
+	scope: CoroutineScope,
+	snackbarHostState: SnackbarHostState,
+	modifier: Modifier,
+	close: () -> Unit
+) {
 	var newEntryNameText by remember { mutableStateOf("") }
 	var newEntryContentText by remember { mutableStateOf("") }
 	var newTagsText by remember { mutableStateOf("") }
 	var newTypeText by remember { mutableStateOf("") }
 
-	Column {
-		Text("Encyclopedia")
+	Column(modifier = modifier.padding(Ui.PADDING)) {
+		Text("Create New Entry")
+
+		TextField(
+			value = newEntryNameText,
+			onValueChange = { newEntryNameText = it },
+			placeholder = { Text("Name") }
+		)
+
 		TextField(
 			value = newEntryNameText,
 			onValueChange = { newEntryNameText = it },
@@ -61,37 +134,13 @@ fun EncyclopediaUi(component: Encyclopedia) {
 				EntryError.NAME_TOO_LONG -> scope.launch { snackbarHostState.showSnackbar("Entry Name was too long") }
 				EntryError.NONE -> {
 					newEntryNameText = ""
+					close()
 					scope.launch { snackbarHostState.showSnackbar("Entry Created") }
 				}
 			}
 		}) {
 			Text("Create")
 		}
-		Spacer(modifier = Modifier)
-
-		Text("Notes")
-		LazyVerticalGrid(
-			columns = GridCells.Adaptive(512.dp),
-			modifier = Modifier.fillMaxWidth(),
-			contentPadding = PaddingValues(Ui.PADDING)
-		) {
-			if (state.entryDefs.isEmpty()) {
-				item {
-					Text("No Entries Found")
-				}
-			} else {
-				items(state.entryDefs.size) { index ->
-					EntryDefItem(
-						entry = state.entryDefs[index],
-						component = component,
-						snackbarHostState = snackbarHostState,
-						scope = scope
-					)
-				}
-			}
-		}
-
-		SnackbarHost(snackbarHostState, modifier = Modifier)
 	}
 }
 

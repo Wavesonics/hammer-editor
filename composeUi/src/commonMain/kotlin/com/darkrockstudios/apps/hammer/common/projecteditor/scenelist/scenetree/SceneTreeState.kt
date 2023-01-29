33164fd04b92108a6b2427421777428072523a2a
Wavesonics <adamwbrown@gmail.com>
2023-01-28 18:55:07 -0800

    SceneListUi now remembers collapsed nodes

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecteditor/scenelist/scenetree/SceneTreeState.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecteditor/scenelist/scenetree/SceneTreeState.kt
@@ -3,6 +3,8 @@ package com.darkrockstudios.apps.hammer.common.projecteditor.scenelist.scenetree
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import com.darkrockstudios.apps.hammer.common.data.InsertPosition
 import com.darkrockstudios.apps.hammer.common.data.MoveRequest
 import com.darkrockstudios.apps.hammer.common.data.SceneItem
@@ -13,126 +15,128 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun rememberReorderableLazyListState(
-    summary: SceneSummary,
-    moveItem: (moveRequest: MoveRequest) -> Unit,
+	summary: SceneSummary,
+	moveItem: (moveRequest: MoveRequest) -> Unit,
 ): SceneTreeState {
-    val coroutineScope = rememberCoroutineScope()
-    val listState = rememberLazyListState()
-    return remember {
-        SceneTreeState(
-            sceneSummary = summary,
-            moveItem = moveItem,
-            coroutineScope = coroutineScope,
-            listState = listState,
-        )
-    }
+	val coroutineScope = rememberCoroutineScope()
+	val listState = rememberLazyListState()
+	val collapsedNodes = rememberSaveable { mutableStateMapOf<Int, Boolean>() }
+	return remember {
+		SceneTreeState(
+			sceneSummary = summary,
+			moveItem = moveItem,
+			coroutineScope = coroutineScope,
+			listState = listState,
+			collapsedNodes = collapsedNodes
+		)
+	}
 }
 
 class SceneTreeState(
-    sceneSummary: SceneSummary,
-    val moveItem: (moveRequest: MoveRequest) -> Unit,
-    val coroutineScope: CoroutineScope,
-    val listState: LazyListState,
+	sceneSummary: SceneSummary,
+	val moveItem: (moveRequest: MoveRequest) -> Unit,
+	val coroutineScope: CoroutineScope,
+	val listState: LazyListState,
+	val collapsedNodes: SnapshotStateMap<Int, Boolean>,
 ) {
-    internal var summary by mutableStateOf(sceneSummary)
-    var selectedId by mutableStateOf(NO_SELECTION)
-    var insertAt by mutableStateOf<InsertPosition?>(null)
-    val collapsedNodes = mutableStateMapOf<Int, Boolean>()
+	internal var summary by mutableStateOf(sceneSummary)
+	var selectedId by mutableStateOf(NO_SELECTION)
+	var insertAt by mutableStateOf<InsertPosition?>(null)
 
-    private var scrollJob by mutableStateOf<Job?>(null)
-    private var treeHash by mutableStateOf(sceneSummary.sceneTree.hashCode())
+	private var scrollJob by mutableStateOf<Job?>(null)
+	private var treeHash by mutableStateOf(sceneSummary.sceneTree.hashCode())
 
-    fun getTree() = summary.sceneTree
+	fun getTree() = summary.sceneTree
 
-    fun updateSummary(sceneSummary: SceneSummary) {
-        if (summary != sceneSummary) {
-            summary = sceneSummary
-            cleanUpOnDelete()
-        }
-    }
+	fun updateSummary(sceneSummary: SceneSummary) {
+		if (summary != sceneSummary) {
+			summary = sceneSummary
+			cleanUpOnDelete()
+		}
+	}
 
-    private fun cleanUpOnDelete() {
-        val newHash = summary.sceneTree.hashCode()
-        if (treeHash != newHash) {
-            treeHash = newHash
+	private fun cleanUpOnDelete() {
+		val newHash = summary.sceneTree.hashCode()
+		if (treeHash != newHash) {
+			treeHash = newHash
 
-            // Prune layouts if the id is not found in the tree
-            val nodeIt = collapsedNodes.iterator()
-            while (nodeIt.hasNext()) {
-                val (id, _) = nodeIt.next()
-                val foundNode = summary.sceneTree.findBy { it.id == id }
-                if (foundNode == null) {
-                    nodeIt.remove()
-                }
-            }
-        }
-    }
+			// Prune layouts if the id is not found in the tree
+			val nodeIt = collapsedNodes.iterator()
+			while (nodeIt.hasNext()) {
+				val (id, _) = nodeIt.next()
+				val foundNode = summary.sceneTree.findBy { it.id == id }
+				if (foundNode == null) {
+					nodeIt.remove()
+				}
+			}
+		}
+	}
 
-    fun collapseAll() {
-        summary.sceneTree
-            .filter { it.value.type == SceneItem.Type.Group }
-            .forEach { node ->
-                collapsedNodes[node.value.id] = true
-            }
-    }
+	fun collapseAll() {
+		summary.sceneTree
+			.filter { it.value.type == SceneItem.Type.Group }
+			.forEach { node ->
+				collapsedNodes[node.value.id] = true
+			}
+	}
 
-    fun expandAll() {
-        collapsedNodes.clear()
-    }
+	fun expandAll() {
+		collapsedNodes.clear()
+	}
 
-    fun autoScroll(up: Boolean) {
-        val previousIndex = (listState.firstVisibleItemIndex - 1).coerceAtLeast(0)
-        if (scrollJob?.isActive != true) {
-            scrollJob = if (up) {
-                if (previousIndex > 0) {
-                    coroutineScope.launch {
-                        listState.animateScrollToItem(previousIndex)
-                    }
-                } else {
-                    null
-                }
-            } else {
-                coroutineScope.launch {
-                    listState.layoutInfo.apply {
-                        val viewportHeight = viewportEndOffset + viewportStartOffset
-                        val index = visibleItemsInfo.size + previousIndex
-                        val lastInfo = visibleItemsInfo[visibleItemsInfo.size - 1]
-                        val offset = lastInfo.size - viewportHeight
+	fun autoScroll(up: Boolean) {
+		val previousIndex = (listState.firstVisibleItemIndex - 1).coerceAtLeast(0)
+		if (scrollJob?.isActive != true) {
+			scrollJob = if (up) {
+				if (previousIndex > 0) {
+					coroutineScope.launch {
+						listState.animateScrollToItem(previousIndex)
+					}
+				} else {
+					null
+				}
+			} else {
+				coroutineScope.launch {
+					listState.layoutInfo.apply {
+						val viewportHeight = viewportEndOffset + viewportStartOffset
+						val index = visibleItemsInfo.size + previousIndex
+						val lastInfo = visibleItemsInfo[visibleItemsInfo.size - 1]
+						val offset = lastInfo.size - viewportHeight
 
-                        listState.animateScrollToItem(index, offset)
-                    }
-                }
-            }
-        }
-    }
+						listState.animateScrollToItem(index, offset)
+					}
+				}
+			}
+		}
+	}
 
-    fun startDragging(id: Int) {
-        if (selectedId == NO_SELECTION) {
-            selectedId = id
-        }
-    }
+	fun startDragging(id: Int) {
+		if (selectedId == NO_SELECTION) {
+			selectedId = id
+		}
+	}
 
-    fun stopDragging() {
-        val insertPosition = insertAt
-        val selectedIndex = summary.sceneTree.indexOf { it.id == selectedId }
-        if (selectedIndex > 0 && insertPosition != null) {
-            val request = MoveRequest(
-                selectedId,
-                insertPosition
-            )
-            moveItem(request)
-        }
+	fun stopDragging() {
+		val insertPosition = insertAt
+		val selectedIndex = summary.sceneTree.indexOf { it.id == selectedId }
+		if (selectedIndex > 0 && insertPosition != null) {
+			val request = MoveRequest(
+				selectedId,
+				insertPosition
+			)
+			moveItem(request)
+		}
 
-        selectedId = NO_SELECTION
-        insertAt = null
-    }
+		selectedId = NO_SELECTION
+		insertAt = null
+	}
 
-    fun toggleExpanded(nodeId: Int) {
-        val collapse = !(collapsedNodes[nodeId] ?: false)
-        collapsedNodes[nodeId] = collapse
-    }
+	fun toggleExpanded(nodeId: Int) {
+		val collapse = !(collapsedNodes[nodeId] ?: false)
+		collapsedNodes[nodeId] = collapse
+	}
 
-    companion object {
-        const val NO_SELECTION = -1
-    }
+	companion object {
+		const val NO_SELECTION = -1
+	}
 }

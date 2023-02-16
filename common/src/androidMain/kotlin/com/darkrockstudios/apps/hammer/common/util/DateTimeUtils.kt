02aabee84d7795d2bab8a833325fd9bbbf7c4a48
Wavesonics <adamwbrown@gmail.com>
2023-02-16 11:46:52 -0700

    Added Date format extension

--- common/src/androidMain/kotlin/com/darkrockstudios/apps/hammer/common/util/DateTimeUtils.kt
+++ common/src/androidMain/kotlin/com/darkrockstudios/apps/hammer/common/util/DateTimeUtils.kt
@@ -1,8 +1,10 @@
 package com.darkrockstudios.apps.hammer.common.util
 
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.*
 import java.time.format.DateTimeFormatter
+
+actual fun Instant.formatLocal(format: String): String =
+	toLocalDateTime(TimeZone.currentSystemDefault()).format(format)
 
 actual fun LocalDateTime.format(format: String): String =
 	DateTimeFormatter.ofPattern(format).format(this.toJavaLocalDateTime())
--- common/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/util/DateTimeUtils.kt
+++ common/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/util/DateTimeUtils.kt
@@ -1,5 +1,7 @@
 package com.darkrockstudios.apps.hammer.common.util
 
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
 
+expect fun Instant.formatLocal(format: String): String
 expect fun LocalDateTime.format(format: String): String

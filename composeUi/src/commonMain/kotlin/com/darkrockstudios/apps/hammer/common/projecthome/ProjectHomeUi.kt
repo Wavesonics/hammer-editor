54c59403bfd2f2f68fd766bdf1a77493e6581ab4
Wavesonics <adamwbrown@gmail.com>
2023-02-16 20:42:13 -0700

    ProjectHome animation work

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecthome/ProjectHomeUi.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projecthome/ProjectHomeUi.kt
@@ -1,8 +1,6 @@
 package com.darkrockstudios.apps.hammer.common.projecthome
 
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -10,6 +8,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -27,150 +26,176 @@ import io.github.koalaplot.core.xychart.*
 
 @Composable
 fun ProjectHomeUi(
-    component: ProjectHome,
+	component: ProjectHome,
 ) {
-    val state by component.state.subscribeAsState()
-    val screen = LocalScreenCharacteristic.current
+	val state by component.state.subscribeAsState()
+	val screen = LocalScreenCharacteristic.current
 
-    if (screen.isWide) {
-        Row(
-            modifier = Modifier.fillMaxSize()
-                .padding(Ui.Padding.XL)
-        ) {
-            Stats(
-                modifier = Modifier.weight(3f).rightBorder(1.dp, MaterialTheme.colorScheme.outline),
-                state = state
-            )
-            Actions(
-                modifier = Modifier.weight(1f),
-                state = state
-            )
-        }
-    } else {
-        Column(
-            modifier = Modifier.fillMaxSize()
-                .padding(Ui.Padding.XL)
-        ) {
-            Stats(
-                modifier = Modifier.fillMaxWidth().bottomBorder(1.dp, MaterialTheme.colorScheme.outline),
-                state = state
-            )
-            Actions(
-                modifier = Modifier.fillMaxWidth(),
-                state = state
-            )
-        }
-    }
+	if (screen.isWide) {
+		Row(
+			modifier = Modifier.fillMaxSize()
+				.padding(Ui.Padding.XL)
+		) {
+			Stats(
+				modifier = Modifier.weight(3f).rightBorder(1.dp, MaterialTheme.colorScheme.outline),
+				state = state
+			)
+			Actions(
+				modifier = Modifier.weight(1f),
+				state = state
+			)
+		}
+	} else {
+		Stats(
+			modifier = Modifier.fillMaxWidth().bottomBorder(1.dp, MaterialTheme.colorScheme.outline),
+			state = state,
+			otherContent = {
+				Actions(
+					modifier = Modifier.fillMaxWidth(),
+					state = state
+				)
+			}
+		)
+	}
 }
 
 @Composable
-private fun Stats(modifier: Modifier, state: ProjectHome.State) {
-    Column(modifier = modifier.fillMaxSize().padding(Ui.Padding.XL)) {
-        Text(
-            state.projectDef.name,
-            style = MaterialTheme.typography.displayMedium,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-        Spacer(modifier = Modifier.size(Ui.Padding.XL))
+private fun Stats(
+	modifier: Modifier,
+	state: ProjectHome.State,
+	otherContent: (@Composable () -> Unit)? = null
+) {
+	Column(modifier = modifier.fillMaxSize().padding(Ui.Padding.XL)) {
+		Text(
+			state.projectDef.name,
+			style = MaterialTheme.typography.displayMedium,
+			color = MaterialTheme.colorScheme.onSurface
+		)
+		Spacer(modifier = Modifier.size(Ui.Padding.XL))
 
-        Text(
-            "Created: ${state.created}",
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-        Spacer(modifier = Modifier.size(Ui.Padding.XL))
+		Text(
+			"Created: ${state.created}",
+			style = MaterialTheme.typography.bodyLarge,
+			color = MaterialTheme.colorScheme.onSurface
+		)
+		Spacer(modifier = Modifier.size(Ui.Padding.XL))
 
-        Text(
-            "Stats:",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.onSurface
-        )
+		Text(
+			"Stats:",
+			style = MaterialTheme.typography.headlineLarge,
+			color = MaterialTheme.colorScheme.onSurface
+		)
 
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(256.dp),
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(Ui.Padding.XL)
-        ) {
-            item {
-                NumericStatsBlock("Scenes", state.numberOfScenes)
-            }
+		LazyVerticalGrid(
+			columns = GridCells.Adaptive(256.dp),
+			modifier = Modifier.fillMaxSize(),
+			contentPadding = PaddingValues(Ui.Padding.XL)
+		) {
+			item {
+				NumericStatsBlock("Scenes", state.numberOfScenes)
+			}
 
-            item {
-                NumericStatsBlock("Total Words", state.totalWords)
-            }
+			item {
+				NumericStatsBlock("Total Words", state.totalWords)
+			}
 
-            item {
-                GenericBlock("Words in Chapters") {
-                    WordsInChaptersChart(Modifier, state)
-                }
-            }
+			item {
+				GenericBlock("Words in Chapters") {
+					WordsInChaptersChart(Modifier, state)
+				}
+			}
 
-            item {
-                GenericBlock("Encyclopedia Entries") {
-                    EncyclopediaChart(Modifier, state)
-                }
-            }
-        }
-    }
+			item {
+				GenericBlock("Encyclopedia Entries") {
+					EncyclopediaChart(Modifier, state)
+				}
+			}
+
+			if (otherContent != null) {
+				item {
+					otherContent()
+				}
+			}
+		}
+	}
 }
 
 @Composable
 private fun NumericStatsBlock(label: String, stateValue: Int) {
-    Card(
-        modifier = Modifier.fillMaxWidth().padding(Ui.Padding.L),
-        elevation = CardDefaults.elevatedCardElevation(Ui.Elevation.MEDIUM)
-    ) {
-        Column(modifier = Modifier.padding(Ui.Padding.L).align(CenterHorizontally)) {
+	Card(
+		modifier = Modifier.fillMaxWidth().padding(Ui.Padding.L),
+		elevation = CardDefaults.elevatedCardElevation(Ui.Elevation.MEDIUM)
+	) {
+		Column(modifier = Modifier.padding(Ui.Padding.L).align(CenterHorizontally)) {
 
-            var value by remember { mutableStateOf(0) }
-            val valueCounter by animateIntAsState(
-                targetValue = stateValue,
-                animationSpec = tween(
-                    durationMillis = 2000,
-                    easing = LinearOutSlowInEasing
-                )
-            )
-            LaunchedEffect(stateValue) {
-                value = 0
-            }
+			var targetScale by remember { mutableStateOf(1f) }
+			var targetValue by remember { mutableStateOf(0) }
 
-            Text(
-                valueCounter.formatDecimalSeparator(),
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.displayMedium,
-                color = MaterialTheme.colorScheme.onSurface,
-                textAlign = TextAlign.Center
-            )
+			val animatedValue by animateIntAsState(
+				targetValue = targetValue,
+				animationSpec = tween(
+					durationMillis = 750,
+					easing = LinearOutSlowInEasing
+				),
+				finishedListener = {
+					targetScale = 1.25f
+				}
+			)
 
-            Text(
-                label,
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onSurface,
-                textAlign = TextAlign.Center
-            )
-        }
-    }
+			val scaleValue by animateFloatAsState(
+				targetValue = targetScale,
+				animationSpec = tween(
+					durationMillis = 250,
+					easing = LinearEasing
+				),
+				finishedListener = {
+					if (targetScale > 1f) {
+						targetScale = 1f
+					}
+				}
+			)
+
+			LaunchedEffect(stateValue) {
+				targetValue = stateValue
+			}
+
+			Text(
+				animatedValue.formatDecimalSeparator(),
+				modifier = Modifier.fillMaxWidth().scale(scaleValue),
+				style = MaterialTheme.typography.displayMedium,
+				color = MaterialTheme.colorScheme.onSurface,
+				textAlign = TextAlign.Center
+			)
+
+			Text(
+				label,
+				modifier = Modifier.fillMaxWidth(),
+				style = MaterialTheme.typography.headlineSmall,
+				color = MaterialTheme.colorScheme.onSurface,
+				textAlign = TextAlign.Center
+			)
+		}
+	}
 }
 
 @Composable
 private fun GenericBlock(label: String, content: @Composable () -> Unit) {
-    Card(
-        modifier = Modifier.fillMaxWidth().padding(Ui.Padding.L),
-        elevation = CardDefaults.elevatedCardElevation(Ui.Elevation.MEDIUM)
-    ) {
-        Column(modifier = Modifier.padding(Ui.Padding.L).align(CenterHorizontally)) {
-            content()
-            Spacer(modifier = Modifier.size(Ui.Padding.L))
-            Text(
-                label,
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onSurface,
-                textAlign = TextAlign.Center
-            )
-        }
-    }
+	Card(
+		modifier = Modifier.fillMaxWidth().padding(Ui.Padding.L),
+		elevation = CardDefaults.elevatedCardElevation(Ui.Elevation.MEDIUM)
+	) {
+		Column(modifier = Modifier.padding(Ui.Padding.L).align(CenterHorizontally)) {
+			content()
+			Spacer(modifier = Modifier.size(Ui.Padding.L))
+			Text(
+				label,
+				modifier = Modifier.fillMaxWidth(),
+				style = MaterialTheme.typography.headlineSmall,
+				color = MaterialTheme.colorScheme.onSurface,
+				textAlign = TextAlign.Center
+			)
+		}
+	}
 }
 
 private val entryTypes = EntryType.values()
@@ -179,88 +204,88 @@ private val colors = generateHueColorPalette(entryTypes.size)
 @OptIn(ExperimentalKoalaPlotApi::class)
 @Composable
 private fun EncyclopediaChart(
-    modifier: Modifier = Modifier,
-    state: ProjectHome.State
+	modifier: Modifier = Modifier,
+	state: ProjectHome.State
 ) {
-    val values =
-        remember(state.encyclopediaEntriesByType) { state.encyclopediaEntriesByType.map { it.value.toFloat() } }
+	val values =
+		remember(state.encyclopediaEntriesByType) { state.encyclopediaEntriesByType.map { it.value.toFloat() } }
 
-    PieChart(
-        modifier = modifier,
-        values = values,
-        label = { index ->
-            Text(
-                entryTypes[index].text,
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-        },
-        labelConnector = { i ->
-            BezierLabelConnector(
-                connectorColor = colors[i],
-                connectorStroke = Stroke(width = 3f)
-            )
-        },
-    )
+	PieChart(
+		modifier = modifier,
+		values = values,
+		label = { index ->
+			Text(
+				entryTypes[index].text,
+				style = MaterialTheme.typography.headlineSmall,
+				color = MaterialTheme.colorScheme.onSurface
+			)
+		},
+		labelConnector = { i ->
+			BezierLabelConnector(
+				connectorColor = colors[i],
+				connectorStroke = Stroke(width = 3f)
+			)
+		},
+	)
 }
 
 @OptIn(ExperimentalKoalaPlotApi::class)
 @Composable
 private fun WordsInChaptersChart(
-    modifier: Modifier = Modifier,
-    state: ProjectHome.State
+	modifier: Modifier = Modifier,
+	state: ProjectHome.State
 ) {
-    val entries = remember(state.wordsByChapter) {
-        state.wordsByChapter.entries.mapIndexed { index, entry ->
-            DefaultBarChartEntry(
-                xValue = index,
-                yMin = 0f,
-                yMax = entry.value.toFloat()
-            )
-        }
-    }
+	val entries = remember(state.wordsByChapter) {
+		state.wordsByChapter.entries.mapIndexed { index, entry ->
+			DefaultBarChartEntry(
+				xValue = index,
+				yMin = 0f,
+				yMax = entry.value.toFloat()
+			)
+		}
+	}
 
-    val xAxis = if (state.wordsByChapter.isNotEmpty()) {
-        List(state.wordsByChapter.keys.size) { i -> i }
-    } else {
-        listOf(0, 100)
-    }
+	val xAxis = if (state.wordsByChapter.isNotEmpty()) {
+		List(state.wordsByChapter.keys.size) { i -> i }
+	} else {
+		listOf(0, 100)
+	}
 
-    val range = remember(state.wordsByChapter) {
-        val entryValues = state.wordsByChapter.entries.map { it.value.toFloat() }
-        if (entryValues.isNotEmpty()) {
-            entryValues.autoScaleRange()
-        } else {
-            listOf(0f, 1f).autoScaleRange()
-        }
-    }
+	val range = remember(state.wordsByChapter) {
+		val entryValues = state.wordsByChapter.entries.map { it.value.toFloat() }
+		if (entryValues.isNotEmpty()) {
+			entryValues.autoScaleRange()
+		} else {
+			listOf(0f, 1f).autoScaleRange()
+		}
+	}
 
-    XYChart(
-        modifier = Modifier.heightIn(64.dp, 196.dp),
-        xAxisModel = CategoryAxisModel(xAxis),
-        yAxisModel = LinearAxisModel(range = range),
-        xAxisTitle = "Chapter",
-        yAxisTitle = "Words",
-        xAxisLabels = { index -> (index + 1).toString() },
-        xAxisStyle = rememberAxisStyle(color = MaterialTheme.colorScheme.onBackground),
-        yAxisLabels = { it.toInt().toString() },
-        yAxisStyle = rememberAxisStyle(color = MaterialTheme.colorScheme.onSurface)
-    ) {
-        VerticalBarChart(series = listOf(entries))
-    }
+	XYChart(
+		modifier = Modifier.heightIn(64.dp, 196.dp),
+		xAxisModel = CategoryAxisModel(xAxis),
+		yAxisModel = LinearAxisModel(range = range),
+		xAxisTitle = "Chapter",
+		yAxisTitle = "Words",
+		xAxisLabels = { index -> (index + 1).toString() },
+		xAxisStyle = rememberAxisStyle(color = MaterialTheme.colorScheme.onBackground),
+		yAxisLabels = { it.toInt().toString() },
+		yAxisStyle = rememberAxisStyle(color = MaterialTheme.colorScheme.onSurface)
+	) {
+		VerticalBarChart(series = listOf(entries))
+	}
 }
 
 @Composable
 private fun Actions(modifier: Modifier, state: ProjectHome.State) {
-    Column(modifier = modifier.padding(Ui.Padding.XL)) {
-        Text(
-            "Actions:",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-        Spacer(modifier = Modifier.size(Ui.Padding.XL))
-        Button(onClick = {}) {
-            Text("Export Story")
-        }
-    }
+	Column(modifier = modifier.padding(Ui.Padding.XL)) {
+		Text(
+			"Actions:",
+			style = MaterialTheme.typography.headlineLarge,
+			color = MaterialTheme.colorScheme.onSurface
+		)
+		Spacer(modifier = Modifier.size(Ui.Padding.XL))
+		Button(onClick = {}) {
+			Text("Export Story")
+		}
+	}
 }

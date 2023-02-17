70f68310b7911050fd73d8546eb7715987ac9235
Bill Booth <cwbooth5@gmail.com>
2023-02-17 14:37:42 -0800

    comment-out failing preview code
    I'm leaving it in here for now so I have the previous implementation to stare at for reference.
    
    This doesn't completely fix the swift-related compile issues, just a bit of them.

--- ios/ios/ui/ProjectEditor/SceneListUi.swift
+++ ios/ios/ui/ProjectEditor/SceneListUi.swift
@@ -37,65 +37,66 @@ struct SceneListUi: View {
     }
 }
 
-struct SceneListUi_Previews: PreviewProvider {
-    static var previews: some View {
-        let lifecycle = LifecycleRegistryKt.LifecycleRegistry()
-        let context = DefaultComponentContext(lifecycle: lifecycle)
-        let fakeFlow = SharedFlowKt.MutableSharedFlow(replay: 1,
-                                                      extraBufferCapacity: 0,
-                                                      onBufferOverflow: BufferOverflow.dropOldest)
-        
-        return SceneListUi(
-            component: SceneListComponent(
-                componentContext: context,
-                projectDef: ProjectDefinition(
-                    name: "Test Proj",
-                    path: HPath(path: "/a/b", isAbsolute: true)),
-                selectedSceneDef: fakeFlow,
-                sceneSelected: { scene in }
-            )
-        )
-    }
-}
+//struct SceneListUi_Previews: PreviewProvider {
+//    static var previews: some View {
+//        let lifecycle = LifecycleRegistryKt.LifecycleRegistry()
+//        let context = DefaultComponentContext(lifecycle: lifecycle)
+//        let fakeFlow = SharedFlowKt.MutableSharedFlow(replay: 1,
+//                                                      extraBufferCapacity: 0,
+//                                                      onBufferOverflow: BufferOverflow.dropOldest)
+//
+//        return SceneListUi(
+//            component: SceneListComponent(
+//                componentContext: context,
+//                projectDef: ProjectDefinition(
+//                    name: "Test Proj",
+//                    path: HPath(path: "/a/b", isAbsolute: true)),
+//                selectedSceneDef: fakeFlow,
+//                sceneSelected: { scene in }
+//            )
+//        )
+//    }
+//}
 
-struct SceneItemUi_Previews: PreviewProvider {
-    static var previews: some View {
-        SceneItemUi(
-            scene: SceneSummary(
-                sceneDef: SceneDefinition(
-                    projectDef: ProjectDefinition(
-                        name: "test prog",
-                        path: HPath(
-                            path: "/a/b",
-                            isAbsolute: false
-                        )
-                    ),
-                    id: 0,
-                    name: "test",
-                    order: 0
-                ),
-                hasDirtyBuffer: false
-            ),
-            onSceneSelected: { scene in }
-        )
-    }
-}
+//struct SceneItemUi_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SceneItemUi(
+//            scene: SceneSummary(
+//                sceneDef: SceneDefinition(
+//                    projectDef: ProjectDefinition(
+//                        name: "test prog",
+//                        path: HPath(
+//                            path: "/a/b",
+//                            isAbsolute: false
+//                        )
+//                    ),
+//                    id: 0,
+//                    name: "test",
+//                    order: 0
+//                ),
+//                hasDirtyBuffer: false
+//            ),
+//            onSceneSelected: { scene in }
+//        )
+//    }
+//}
 
 struct SceneItemUi: View {
     
     private var sceneSummary: SceneSummary
     
-    private var onSceneSelected: (SceneDefinition) -> Void
+    private var onSceneSelected: (SceneItem) -> Void
     
-    init(scene: SceneSummary, onSceneSelected: @escaping (SceneDefinition) -> Void) {
+    init(scene: SceneSummary, onSceneSelected: @escaping (SceneItem) -> Void) {
         self.sceneSummary = scene
         self.onSceneSelected = onSceneSelected
     }
     
     var body: some View {
-        Text("Row \(sceneSummary.sceneDef.name)")
-            .onTapGesture {
-                onSceneSelected(sceneSummary.sceneDef)
-            }
+//        Text("Row \(sceneSummary.sceneDef.name)")
+//            .onTapGesture {
+//                onSceneSelected(sceneSummary.sceneDef)
+//            }
+        Text("TODO")
     }
 }

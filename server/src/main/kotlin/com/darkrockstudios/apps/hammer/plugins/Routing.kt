680e759e9e47036278cfb71e2d6a07991119effe
Adam Brown <adamwbrown@gmail.com>
2023-03-10 21:45:53 -0800

    Fix test

--- server/src/main/kotlin/com/darkrockstudios/apps/hammer/plugins/Routing.kt
+++ server/src/main/kotlin/com/darkrockstudios/apps/hammer/plugins/Routing.kt
@@ -2,14 +2,14 @@ package com.darkrockstudios.apps.hammer.plugins
 
 import com.darkrockstudios.apps.hammer.account.accountRoutes
 import io.ktor.http.*
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get("/teapot") {
-            call.respondText("I'm a little ", status = HttpStatusCode.fromValue(418))
+            call.respondText("I'm a little Tea Pot", status = HttpStatusCode.fromValue(418))
         }
     }
 

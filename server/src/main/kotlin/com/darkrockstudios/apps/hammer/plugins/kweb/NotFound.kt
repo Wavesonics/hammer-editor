d7a93605da874281a0c08f2408468f393a918a6c
Wavesonics <adamwbrown@gmail.com>
2023-04-20 22:58:43 -0700

    Minor 404 update

--- server/src/main/kotlin/com/darkrockstudios/apps/hammer/plugins/kweb/NotFound.kt
+++ server/src/main/kotlin/com/darkrockstudios/apps/hammer/plugins/kweb/NotFound.kt
@@ -6,9 +6,8 @@ import kweb.plugins.fomanticUI.fomantic
 import kweb.routing.NotFoundReceiver
 
 val notFoundPage: NotFoundReceiver = { path ->
-	div(fomantic.ui.text.container) {
-		div(fomantic.ui.middle.aligned.centered.aligned.padded.grid)
-		{
+	div(fomantic.ui.middle.aligned.center.aligned.grid) {
+		div(fomantic.column) {
 			div(fomantic.ui.text.message.error) {
 				h2().text("404 Not Found")
 			}

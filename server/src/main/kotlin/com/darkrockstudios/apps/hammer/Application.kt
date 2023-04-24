8b898916975a94bb0673fcecf4a856a4c8b0fa29
Adam Brown <adamwbrown@gmail.com>
2023-04-23 21:56:14 -0700

    SSL work
    SSL work
    
    SSL work
    
    SSL work
    
    SSL work 2
    
    SSL work 2
    
    SSL work

--- server/src/main/kotlin/com/darkrockstudios/apps/hammer/Application.kt
+++ server/src/main/kotlin/com/darkrockstudios/apps/hammer/Application.kt
@@ -7,30 +7,86 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
+import java.io.File
+import java.security.KeyStore
 
 fun main(args: Array<String>) {
-    val parser = ArgParser("server")
-    val portArg by parser.option(ArgType.Int, shortName = "p", fullName = "", description = "Port")
+	val parser = ArgParser("server")
+	val portArg by parser.option(ArgType.Int, shortName = "p", fullName = "port", description = "Port")
+	val certPathArg by parser.option(
+		ArgType.String,
+		shortName = "c",
+		fullName = "sslCert",
+		description = "SSL Cert Path"
+	)
+	val certPasswordArg by parser.option(
+		ArgType.String,
+		shortName = "x",
+		fullName = "sslPassword",
+		description = "SSL Cert Password"
+	)
+	val certAliasArg by parser.option(
+		ArgType.String,
+		shortName = "z",
+		fullName = "sslAlias",
+		description = "SSL Cert Key Alias"
+	)
 
-    parser.parse(args)
+	parser.parse(args)
 
-    val port = portArg ?: 8080
+	val port = portArg ?: 8080
+	println(certPathArg)
+	val keyStoreFile: File? = certPathArg?.let { return@let File(it) }
 
-    embeddedServer(
-        Netty,
-        port = port,
-        host = "0.0.0.0",
-        module = Application::appMain,
-        watchPaths = listOf("classes")
-    ).start(wait = true)
+	startServer(port, keyStoreFile, certPasswordArg, certAliasArg)
+}
+
+private fun startServer(httpPort: Int, keyStoreFile: File?, certPasswordArg: String?, certAliasArg: String?) {
+	val password = certPasswordArg ?: ""
+	val keyStore = getKeyStore(keyStoreFile, certPasswordArg)
+
+	val environment = applicationEngineEnvironment {
+		connector {
+			port = httpPort
+			host = "0.0.0.0"
+		}
+
+		if (keyStore != null) {
+			sslConnector(
+				keyStore = keyStore,
+				keyAlias = certAliasArg ?: "",
+				keyStorePassword = { password.toCharArray() },
+				privateKeyPassword = { password.toCharArray() }) {
+				keyStorePath = keyStoreFile
+				host = "0.0.0.0"
+			}
+		}
+
+		module(Application::appMain)
+		watchPaths = listOf("classes")
+	}
+
+	embeddedServer(
+		Netty,
+		environment = environment
+	).start(wait = true)
+}
+
+private fun getKeyStore(certFile: File?, certPasswordArg: String?): KeyStore? {
+	return if (certFile != null) {
+		if (certFile.exists().not()) throw IllegalArgumentException("SSL Cert not found")
+		KeyStore.getInstance(certFile, certPasswordArg?.toCharArray())
+	} else {
+		null
+	}
 }
 
 fun Application.appMain() {
-    configureDependencyInjection()
-    configureSerialization()
-    configureMonitoring()
-    configureHTTP()
-    configureSecurity()
-    configureRouting()
-    configureKweb()
+	configureDependencyInjection()
+	configureSerialization()
+	configureMonitoring()
+	configureHTTP()
+	configureSecurity()
+	configureRouting()
+	configureKweb()
 }

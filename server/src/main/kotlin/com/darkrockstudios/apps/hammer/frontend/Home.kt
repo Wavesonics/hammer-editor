46e2ee65b477df861ddbffb808cf28b699320efc
Adam Brown <adamwbrown@gmail.com>
2023-04-26 17:32:15 -0700

    A little more work on server Home page

--- server/src/main/kotlin/com/darkrockstudios/apps/hammer/frontend/Home.kt
+++ server/src/main/kotlin/com/darkrockstudios/apps/hammer/frontend/Home.kt
@@ -17,131 +17,131 @@ fun RouteReceiver.homePage(
 	goTo: (String) -> Unit,
 ) {
 	path("/") {
-		val whiteList = runBlocking { whiteListEnabled.useWhiteList() }
-
 		div(fomantic.pusher) {
 			mastHead(goTo)
 
 			div(fomantic.ui.vertical.segment.padded) {
 				div(fomantic.ui.middle.aligned.stackable.grid.container) {
+					serverMessage(config, whiteListEnabled)
 
-					if(whiteList || config.serverMessage.isNotBlank()) {
-						div(fomantic.ui.row.text.container) {
-							div(fomantic.ui.message.warning) {
+					whatIsHammer()
 
-								if (config.serverMessage.isNotBlank()) {
-									div(fomantic.ui.center.aligned.row) {
-										div(fomantic.ui.internally.celled.grid) {
-											div(fomantic.center.aligned.row) {
-												if (config.serverMessage.isNotBlank()) {
-													div(fomantic.column) {
-														h3(fomantic.ui.header).text("Server Message")
-														p().innerHTML(config.serverMessage)
-													}
-												}
-											}
+					hammerFeatures()
+
+					downloadHammer()
+				}
+			}
+
+			footer()
+		}
+	}
+}
+
+private fun Component.serverMessage(config: ServerConfig, whiteListEnabled: WhiteListRepository) {
+	val whiteList = runBlocking { whiteListEnabled.useWhiteList() }
+
+	if (whiteList || config.serverMessage.isNotBlank()) {
+		div(fomantic.ui.row.text.container) {
+			div(fomantic.padded) {
+				div(fomantic.ui.message.warning) {
+					if (config.serverMessage.isNotBlank()) {
+						div(fomantic.ui.center.aligned.row) {
+							div(fomantic.ui.internally.celled.grid) {
+								div(fomantic.center.aligned.row) {
+									if (config.serverMessage.isNotBlank()) {
+										div(fomantic.column) {
+											h3(fomantic.ui.header).text("Server Message")
+											p().innerHTML(config.serverMessage)
 										}
 									}
 								}
-
-								if (whiteList) {
-									div(fomantic.ui.row) {
-										div(fomantic.ui.info.message.column).innerHTML("This server requires you to get permission before creating an account. Please contact ${config.contact} to get on the white list.")
-									}
-								}
 							}
 						}
 					}
 
-					div(fomantic.ui.text.container.row) {
-						h1(fomantic.ui.header).text("What is Hammer?")
-						p().innerHTML("Hammer is an open source tool for writing stories, and the worlds in which they exist.")
-					}
-
-					div(fomantic.ui.row.four.column.grid) {
-						div(fomantic.row) {
-							messageColumn(
-								"Multi-platform",
-								"This app is where ever you are. Your phone, tablet, desktop, laptop, this program can be installed and run, not simply a website-in-a-box, but instead using native client side technologies to provide the best experience possible."
-							)
-
-							messageColumn(
-								"Multi-Screen",
-								"Whether you are on a phone, tablet, or desktop, Hammer will make the best use of your screen space. Also supporting both light and dark modes."
-							)
-
-							messageColumn(
-								"Offline first",
-								"I was frustrated with most of the story writing software I was finding as they were using web technologies (aka: Web Page in a box) which always seems to run into problems while being used offline for long periods of time. Hammer is designed from the ground up to be entirely local, no internet connection required, ever."
-							)
-
-							messageColumn(
-								"Transparent Data",
-								"Your data is yours. It's not stored in the cloud, or some opaque database. It is stored in simple, human readable files, just using files and folders to define the project structure. You can open your OSes file browser and take a look for your self. If this program went away today you would be able to easily interact with your data."
-							)
-						}
-						/*
-						div(fomantic.row) {
-							messageColumn(
-								"Intelligent Syncing",
-								"Your data can be synchronized between devices allowing you to work on your story from anywhere, and have no fear of a change on one device, overwriting a change on another device.")
-
-							messageColumn(
-								"Self hosted Syncing",
-								"Syncing is entirely optional, you can install the client and use it on one device, or you can install the server and sync your data between devices. You could even use some other service to sync your data, like Dropbox or Google Drive. It's all up to you.")
-						}
-						*/
-					}
-
-					div(fomantic.ui.text.container.row) {
-						h1(fomantic.ui.dividing.header).text("Get your Hammer:")
-					}
-
-					div(fomantic.ui.row.four.column.grid) {
-						div(fomantic.row.middle.aligned) {
-							downloadColumn(
-								"Windows",
-								"windows",
-								"https://github.com/Wavesonics/hammer-editor/releases/latest/download/hammer.msi"
-							)
-
-							downloadColumn(
-								"Linux",
-								"linux",
-								"https://github.com/Wavesonics/hammer-editor/releases/latest/download/hammer_amd64.deb "
-							)
-
-							downloadColumn(
-								"Android",
-								"android",
-								"https://play.google.com/store/apps/details?id=com.darkrockstudios.apps.hammer.android"
-							)
-
-							div(fomantic.ui.three.wide.right.floated.column) {
-								img(fomantic.ui.large.image).src("/assets/images/download.png")
-							}
+					if (whiteList) {
+						div(fomantic.ui.row) {
+							div(fomantic.ui.info.message.column).innerHTML("This server requires you to get permission before creating an account. Please contact ${config.contact} to get on the white list.")
 						}
 					}
 				}
 			}
+		}
+	}
+}
 
-			div(fomantic.ui.inverted.vertical.segment) {
-				div(fomantic.ui.center.aligned.text.container) {
-					div(fomantic.row) {
-						a(href = "https://github.com/Wavesonics/hammer-editor/") {
-							i(fomantic.icon).addClasses("github")
-							span().text("GitHub")
-						}
+private fun Component.whatIsHammer() {
+	div(fomantic.ui.text.container.row) {
+		div(fomantic.padded) {
+			h1(fomantic.ui.header).text("What is Hammer?")
+			p().innerHTML("Hammer is an open source tool for writing stories, and the worlds in which they exist.")
+		}
+	}
+}
 
-						span().text(" ")
+private fun Component.hammerFeatures() {
+	div(fomantic.ui.row.four.column.grid) {
+		messageColumn(
+			"Multi-platform",
+			"This app is where ever you are. Your phone, tablet, desktop, laptop, this program can be installed and run, not simply a website-in-a-box, but instead using native client side technologies to provide the best experience possible."
+		)
 
-						a(href = "https://discord.gg/GTmgjZcupk") {
-							i(fomantic.icon).addClasses("discord")
-							span().text("Discord")
-						}
-					}
-				}
-			}.addClasses("footer")
+		messageColumn(
+			"Multi-Screen",
+			"Whether you are on a phone, tablet, or desktop, Hammer will make the best use of your screen space. Also supporting both light and dark modes."
+		)
+
+		messageColumn(
+			"Offline first",
+			"I was frustrated with most of the story writing software I was finding as they were using web technologies (aka: Web Page in a box) which always seems to run into problems while being used offline for long periods of time. Hammer is designed from the ground up to be entirely local, no internet connection required, ever."
+		)
+
+		messageColumn(
+			"Transparent Data",
+			"Your data is yours. It's not stored in the cloud, or some opaque database. It is stored in simple, human readable files, just using files and folders to define the project structure. You can open your OSes file browser and take a look for your self. If this program went away today you would be able to easily interact with your data."
+		)
+		/*
+		div(fomantic.row) {
+			messageColumn(
+				"Intelligent Syncing",
+				"Your data can be synchronized between devices allowing you to work on your story from anywhere, and have no fear of a change on one device, overwriting a change on another device.")
+
+			messageColumn(
+				"Self hosted Syncing",
+				"Syncing is entirely optional, you can install the client and use it on one device, or you can install the server and sync your data between devices. You could even use some other service to sync your data, like Dropbox or Google Drive. It's all up to you.")
+		}
+		*/
+	}
+}
+
+private fun Component.downloadHammer() {
+	div(fomantic.ui.text.container.row) {
+		div(fomantic.padded) {
+			h1(fomantic.ui.dividing.header).text("Get your Hammer:")
+		}
+	}
+
+	div(fomantic.ui.row.four.column.grid.middle.aligned) {
+		downloadColumn(
+			"Windows",
+			"windows",
+			"https://github.com/Wavesonics/hammer-editor/releases/latest/download/hammer.msi"
+		)
+
+		downloadColumn(
+			"Linux",
+			"linux",
+			"https://github.com/Wavesonics/hammer-editor/releases/latest/download/hammer_amd64.deb "
+		)
+
+		downloadColumn(
+			"Android",
+			"android",
+			"https://play.google.com/store/apps/details?id=com.darkrockstudios.apps.hammer.android"
+		)
+
+		div(fomantic.ui.three.wide.right.floated.column) {
+			img(fomantic.ui.large.image).src("/assets/images/download.png")
 		}
 	}
 }
@@ -190,4 +190,24 @@ private fun Component.menu(goTo: (String) -> Unit) {
 			}
 		}
 	}
+}
+
+private fun Component.footer() {
+	div(fomantic.ui.inverted.vertical.segment) {
+		div(fomantic.ui.center.aligned.text.container) {
+			div(fomantic.row) {
+				a(href = "https://github.com/Wavesonics/hammer-editor/") {
+					i(fomantic.icon).addClasses("github")
+					span().text("GitHub")
+				}
+
+				span().text(" ")
+
+				a(href = "https://discord.gg/GTmgjZcupk") {
+					i(fomantic.icon).addClasses("discord")
+					span().text("Discord")
+				}
+			}
+		}
+	}.addClasses("footer")
 }

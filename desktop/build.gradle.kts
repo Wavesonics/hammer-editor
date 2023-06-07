e4aa92de64184f5365fc8f508f4cbebec98f413e
Wavesonics <adamwbrown@gmail.com>
2023-06-06 22:03:53 -0700

    Added license info to distributions

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -5,10 +5,10 @@ val data_version: String by extra
 val jvm_version: String by extra
 
 plugins {
-    kotlin("multiplatform")
-    kotlin("plugin.serialization")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlinx.kover")
+	kotlin("multiplatform")
+	kotlin("plugin.serialization")
+	id("org.jetbrains.compose")
+	id("org.jetbrains.kotlinx.kover")
 }
 
 group = "com.darkrockstudios.apps.hammer.desktop"
@@ -16,49 +16,60 @@ version = app_version
 
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = jvm_version
-        }
-        withJava()
-    }
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                implementation(project(":base"))
-                implementation(project(":common"))
-                implementation(project(":composeUi"))
-                implementation(compose.preview)
-                implementation(compose.desktop.currentOs)
-                implementation("com.github.weisj:darklaf-core:3.0.2")
-                implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
-            }
-        }
-        val jvmTest by getting
-    }
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = jvm_version
+		}
+		withJava()
+	}
+	sourceSets {
+		val jvmMain by getting {
+			dependencies {
+				implementation(project(":base"))
+				implementation(project(":common"))
+				implementation(project(":composeUi"))
+				implementation(compose.preview)
+				implementation(compose.desktop.currentOs)
+				implementation("com.github.weisj:darklaf-core:3.0.2")
+				implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+			}
+		}
+		val jvmTest by getting
+	}
 }
 
 compose.desktop {
-    application {
-        mainClass = "com.darkrockstudios.apps.hammer.desktop.MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            includeAllModules = true
-            packageName = "hammer"
-            packageVersion = app_version
-            outputBaseDir.set(project.buildDir.resolve("installers"))
+	application {
+		mainClass = "com.darkrockstudios.apps.hammer.desktop.MainKt"
+		nativeDistributions {
+			targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+			includeAllModules = true
+			packageName = "hammer"
+			packageVersion = app_version
+			description = "A simple tool for building stories."
+			copyright = "© 2023 Adam W. Brown, All rights reserved."
+			licenseFile.set(project.file("../LICENSE"))
+			outputBaseDir.set(project.buildDir.resolve("installers"))
 
-            windows.apply {
-                menuGroup = "Hammer"
-                shortcut = true
-                console = true
-            }
-        }
-        jvmArgs("-Dcompose.application.configure.swing.globals=false")
+			windows.apply {
+				menuGroup = "Hammer"
+				shortcut = true
+				console = true
+			}
 
-        buildTypes.release.proguard {
-            isEnabled.set(false)
-            configurationFiles.from("proguard-rules.pro")
-        }
-    }
+			linux.apply {
+				rpmLicenseType = "MIT"
+			}
+
+			macOS.apply {
+				appStore = false
+			}
+		}
+		jvmArgs("-Dcompose.application.configure.swing.globals=false")
+
+		buildTypes.release.proguard {
+			isEnabled.set(false)
+			configurationFiles.from("proguard-rules.pro")
+		}
+	}
 }

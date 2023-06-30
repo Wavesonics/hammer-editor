56b4f4ae69531905097bbae3557dd8a110ee0be6
Wavesonics <adamwbrown@gmail.com>
2023-06-29 20:42:22 -0700

    A little UI work on Timeline create screen

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/timeline/CreateTimeLineEventUi.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/timeline/CreateTimeLineEventUi.kt
@@ -1,13 +1,11 @@
 package com.darkrockstudios.apps.hammer.common.timeline
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -37,48 +35,52 @@ fun CreateTimeLineEventUi(
 	val screen = LocalScreenCharacteristic.current
 	val needsExplicitClose = remember { screen.needsExplicitClose }
 
-	Column(modifier = modifier) {
-		if (needsExplicitClose) {
-			IconButton(
-				onClick = close,
-				modifier = Modifier.align(End).padding(Ui.Padding.XL),
-			) {
-				Icon(
-					Icons.Default.Close,
-					MR.strings.timeline_create_close_button.get(),
-					tint = MaterialTheme.colorScheme.onBackground
+	Box(modifier = modifier.fillMaxSize()) {
+		Card(modifier = Modifier.padding(Ui.Padding.XL).widthIn(max = 512.dp).align(Center)) {
+			Column(modifier = Modifier.padding(Ui.Padding.L)) {
+				if (needsExplicitClose) {
+					IconButton(
+						onClick = close,
+						modifier = Modifier.align(End).padding(Ui.Padding.XL),
+					) {
+						Icon(
+							Icons.Default.Close,
+							MR.strings.timeline_create_close_button.get(),
+							tint = MaterialTheme.colorScheme.onBackground
+						)
+					}
+				}
+				Text(
+					MR.strings.timeline_create_title.get(),
+					style = MaterialTheme.typography.headlineLarge,
+					color = MaterialTheme.colorScheme.onBackground
 				)
-			}
-		}
-		Text(
-			MR.strings.timeline_create_title.get(),
-			style = MaterialTheme.typography.headlineLarge,
-			color = MaterialTheme.colorScheme.onBackground
-		)
-		TextField(
-			value = dateText,
-			onValueChange = { dateText = it },
-			label = { Text(MR.strings.timeline_create_date_label.get()) },
-			singleLine = true
-		)
-		OutlinedTextField(
-			modifier = Modifier.fillMaxWidth().height(128.dp),
-			value = contentText,
-			onValueChange = { contentText = it },
-			label = { Text(MR.strings.timeline_create_content_label.get()) },
-		)
+				TextField(
+					value = dateText,
+					onValueChange = { dateText = it },
+					label = { Text(MR.strings.timeline_create_date_label.get()) },
+					singleLine = true
+				)
+				OutlinedTextField(
+					modifier = Modifier.fillMaxWidth().height(128.dp),
+					value = contentText,
+					onValueChange = { contentText = it },
+					label = { Text(MR.strings.timeline_create_content_label.get()) },
+				)
 
-		Button(onClick = {
-			scope.launch {
-				if (component.createEvent(dateText, contentText)) {
-					launch { snackbarHostState.showSnackbar(strRes.get(MR.strings.timeline_create_toast_success)) }
-					close()
-				} else {
-					launch { snackbarHostState.showSnackbar(strRes.get(MR.strings.timeline_create_toast_failure)) }
+				Button(onClick = {
+					scope.launch {
+						if (component.createEvent(dateText, contentText)) {
+							launch { snackbarHostState.showSnackbar(strRes.get(MR.strings.timeline_create_toast_success)) }
+							close()
+						} else {
+							launch { snackbarHostState.showSnackbar(strRes.get(MR.strings.timeline_create_toast_failure)) }
+						}
+					}
+				}) {
+					Text(MR.strings.timeline_create_create_event_button.get())
 				}
 			}
-		}) {
-			Text(MR.strings.timeline_create_create_event_button.get())
 		}
 	}
 }

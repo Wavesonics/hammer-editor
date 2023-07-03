b7545db6c1aa69594c80fa12a71450e7570ebaf1
Wavesonics <adamwbrown@gmail.com>
2023-07-03 14:58:52 -0700

    More Material 3 work on ProjectSelect

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/compose/UiConstants.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/compose/UiConstants.kt
@@ -1,5 +1,6 @@
 package com.darkrockstudios.apps.hammer.common.compose
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.ui.unit.dp
 
 object Ui {
@@ -9,11 +10,24 @@ object Ui {
 		val LARGE = 12.dp
 	}
 
+	object ToneElevation {
+		val NONE = 0.dp
+		val SMALL = 1.dp
+		val MEDIUM = 2.dp
+		val LARGE = 4.dp
+	}
+
 	object Padding {
 		val S = 4.dp
 		val M = 8.dp
 		val L = 12.dp
 		val XL = 16.dp
+
+		val contents = PaddingValues(
+			top = XL,
+			start = XL,
+			end = XL,
+		)
 	}
 
 	object NavDrawer {

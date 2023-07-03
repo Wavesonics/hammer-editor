70cda4fd7fbb57add8d80172a9d7db37c557af19
Wavesonics <adamwbrown@gmail.com>
2023-07-02 22:30:22 -0700

    Began UI overhaul! ProjectSelectionUI now using Material 3 WindowSizes

---
+++ android/src/main/kotlin/com/darkrockstudios/apps/hammer/android/preview/ProjectSelectActivity.Preview.kt
@@ -0,0 +1,57 @@
+package com.darkrockstudios.apps.hammer.android.preview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.arkivanov.decompose.Child.Created
+import com.arkivanov.decompose.router.slot.ChildSlot
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.Value
+import com.darkrockstudios.apps.hammer.android.Content
+import com.darkrockstudios.apps.hammer.common.components.projecteditor.metadata.ProjectMetadata
+import com.darkrockstudios.apps.hammer.common.components.projectselection.ProjectSelection
+import com.darkrockstudios.apps.hammer.common.components.projectselection.projectslist.ProjectsList
+import com.darkrockstudios.apps.hammer.common.data.ProjectDef
+import com.darkrockstudios.apps.hammer.common.fileio.HPath
+
+
+val projectListComponent = object : ProjectsList {
+	override val state: Value<ProjectsList.State> =
+		MutableValue(
+			ProjectsList.State(
+				projectsPath = HPath("/", "root", true)
+			)
+		)
+
+	override fun loadProjectList() {}
+	override fun selectProject(projectDef: ProjectDef) {}
+	override fun showCreate() {}
+	override fun hideCreate() {}
+	override fun createProject(projectName: String) {}
+	override fun deleteProject(projectDef: ProjectDef) {}
+	override fun syncProjects(callback: (Boolean) -> Unit) {}
+	override fun showProjectsSync() {}
+	override fun hideProjectsSync() {}
+	override fun cancelProjectsSync() {}
+	override suspend fun loadProjectMetadata(projectDef: ProjectDef): ProjectMetadata? = null
+	override fun onProjectNameUpdate(newProjectName: String) {}
+}
+
+val component = object : ProjectSelection {
+	override val slot: Value<ChildSlot<ProjectSelection.Config, ProjectSelection.Destination>> =
+		MutableValue<ChildSlot<ProjectSelection.Config, ProjectSelection.Destination>>(
+			ChildSlot(
+				Created(
+					configuration = ProjectSelection.Config.ProjectsList,
+					instance = ProjectSelection.Destination.ProjectsListDestination(projectListComponent)
+				)
+			)
+		)
+	override val showProjectDirectory = true
+	override fun showLocation(location: ProjectSelection.Locations) {}
+}
+
+@Preview
+@Composable
+private fun ProjectSelectActivityPreview() {
+	Content(component)
+}

cf28fa2dfd3cff8447bd46dafb99d1fd34fbcd51
Wavesonics <adamwbrown@gmail.com>
2023-08-15 22:27:37 -0700

    Improved Encyclopedia conflict ui a bit

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projectsync/EncyclopediaEntryConflict.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/projectsync/EncyclopediaEntryConflict.kt
@@ -1,5 +1,6 @@
 package com.darkrockstudios.apps.hammer.common.projectsync
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
@@ -9,6 +10,8 @@ import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.arkivanov.decompose.value.MutableValue
 import com.darkrockstudios.apps.hammer.MR
 import com.darkrockstudios.apps.hammer.base.http.ApiProjectEntity
 import com.darkrockstudios.apps.hammer.common.components.projectsync.ProjectSync
@@ -100,19 +103,59 @@ private fun RemoteEntry(
 				Text(MR.strings.sync_conflict_remote_use_button.get())
 			}
 		}
-		SelectionContainer {
+
+		Row {
 			Text(
-				entityConflict.serverEntity.name,
+				modifier = Modifier.padding(end = Ui.Padding.M),
+				text = MR.strings.sync_conflict_encyclopedia_label_name.get(),
+				style = MaterialTheme.typography.bodyLarge,
+				fontWeight = FontWeight.Bold
+			)
+
+			SelectionContainer {
+				Text(
+					entityConflict.serverEntity.name,
+					style = MaterialTheme.typography.bodyLarge
+				)
+			}
+		}
+
+		Row {
+			Text(
+				modifier = Modifier.padding(end = Ui.Padding.M),
+				text = MR.strings.sync_conflict_encyclopedia_label_type.get(),
+				style = MaterialTheme.typography.bodyLarge,
+				fontWeight = FontWeight.Bold
+			)
+
+			Text(
+				text = entityConflict.serverEntity.entryType,
+				style = MaterialTheme.typography.bodyLarge,
+			)
+		}
+
+		Row {
+			Text(
+				modifier = Modifier.padding(end = Ui.Padding.M),
+				text = MR.strings.sync_conflict_encyclopedia_label_image.get(),
+				style = MaterialTheme.typography.bodyLarge,
+				fontWeight = FontWeight.Bold
+			)
+
+			Text(
+				if (entityConflict.serverEntity.image != null)
+					MR.strings.sync_conflict_encyclopedia_has_image.get()
+				else
+					MR.strings.sync_conflict_encyclopedia_no_image.get(),
 				style = MaterialTheme.typography.bodyLarge
 			)
 		}
-		Text(entityConflict.serverEntity.entryType)
+
 		Text(
-			if (entityConflict.serverEntity.image != null)
-				MR.strings.sync_conflict_encyclopedia_has_image.get()
-			else
-				MR.strings.sync_conflict_encyclopedia_no_image.get(),
-			style = MaterialTheme.typography.bodyLarge
+			modifier = Modifier.padding(end = Ui.Padding.M),
+			text = MR.strings.sync_conflict_encyclopedia_label_content.get(),
+			style = MaterialTheme.typography.bodyLarge,
+			fontWeight = FontWeight.Bold
 		)
 		SelectionContainer {
 			Text(
@@ -126,4 +169,33 @@ private fun RemoteEntry(
 			}
 		}
 	}
+}
+
+@Preview
+@Composable
+private fun RemotePreview() {
+	val entity = ApiProjectEntity.EncyclopediaEntryEntity(
+		id = 1,
+		name = "Test",
+		entryType = "person",
+		text = "Test, Test, Test, Test, Test,Test,v,v,v,v,v,",
+		tags = listOf("tag1", "tag2"),
+		image = null
+	)
+
+	RemoteEntry(
+		entityConflict = ProjectSync.EntityConflict.EncyclopediaEntryConflict(entity, entity),
+		component = object : ProjectSync {
+			override val state = MutableValue(
+				ProjectSync.State(
+				)
+			)
+
+			override fun syncProject(onComplete: (Boolean) -> Unit) {}
+			override fun resolveConflict(resolvedEntity: ApiProjectEntity) {}
+			override fun endSync() {}
+			override fun cancelSync() {}
+			override fun showLog(show: Boolean) {}
+		}
+	)
 }

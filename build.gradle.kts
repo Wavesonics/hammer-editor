b39090d4dd00d4dc21bcf791644caa2d939311eb
Wavesonics <adamwbrown@gmail.com>
2023-08-16 21:12:24 -0700

    Remove jetbrains maven only need this for dev releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jitpack.io")
     }
 }

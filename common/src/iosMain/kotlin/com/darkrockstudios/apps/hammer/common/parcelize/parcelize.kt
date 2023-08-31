3c0686fcc0cda46dfa32ab4c2d5de1923d76fcad
Wavesonics <adamwbrown@gmail.com>
2023-08-31 16:31:24 -0700

    Possible iOS fix?

--- common/src/iosMain/kotlin/com/darkrockstudios/apps/hammer/common/parcelize/parcelize.kt
+++ common/src/iosMain/kotlin/com/darkrockstudios/apps/hammer/common/parcelize/parcelize.kt
@@ -3,7 +3,18 @@ package com.darkrockstudios.apps.hammer.common.parcelize
 import com.arkivanov.essenty.parcelable.Parceler
 import dev.icerock.moko.resources.StringResource
 import kotlinx.datetime.Instant
+import platform.Foundation.NSCoder
 
 internal actual object InstantParceler : Parceler<Instant>
 
-internal actual object StringResourceParceler : Parceler<StringResource>
+internal actual object StringResourceParceler : Parceler<StringResource> {
+	override fun create(coder: NSCoder): StringResource {
+		val id = coder.decodeStringForKey(key = "stringResource")
+		return StringResource(resourceId = id)
+	}
+
+
+	override fun StringResource.write(coder: NSCoder) {
+		coder.encodeString(value = resourceId, forKey = "stringResource")
+	}
+}

532fcafae674049faa409d611e02320020320ea0
Wavesonics <adamwbrown@gmail.com>
2023-09-02 01:06:49 -0700

    Maybe nicer TOML encoding?

--- common/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/data/encyclopediarepository/entry/EntryContent.kt
+++ common/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/data/encyclopediarepository/entry/EntryContent.kt
@@ -2,12 +2,14 @@ package com.darkrockstudios.apps.hammer.common.data.encyclopediarepository.entry
 
 import com.darkrockstudios.apps.hammer.common.data.ProjectDef
 import kotlinx.serialization.Serializable
+import net.peanuuutz.tomlkt.TomlMultilineString
 
 @Serializable
 data class EntryContent(
 	val id: Int,
 	val name: String,
 	val type: EntryType,
+	@TomlMultilineString
 	val text: String,
 	val tags: Set<String>
 ) {

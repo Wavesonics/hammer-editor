8051254d4e7dc96132f868eadf02952d80f42031
Wavesonics <adamwbrown@gmail.com>
2023-09-19 21:11:54 -0700

    Fix About screen links on Android

--- common/src/androidMain/kotlin/com/darkrockstudios/apps/hammer/common/util/UrlLauncher.kt
+++ common/src/androidMain/kotlin/com/darkrockstudios/apps/hammer/common/util/UrlLauncher.kt
@@ -2,12 +2,14 @@ package com.darkrockstudios.apps.hammer.common.util
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 
 
 class UrlLauncherAndroid(private val context: Context) : UrlLauncher {
 	override fun openInBrowser(url: String) {
 		val intent = Intent(Intent.ACTION_VIEW)
+		intent.flags = FLAG_ACTIVITY_NEW_TASK
 		intent.data = Uri.parse(url)
 		context.startActivity(intent)
 	}

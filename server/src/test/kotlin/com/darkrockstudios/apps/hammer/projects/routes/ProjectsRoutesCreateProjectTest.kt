acd60d011987cb40495c88841f9706e58136cdca
Adam Brown <adamwbrown@gmail.com>
2024-06-29 23:17:53 -0700

    Working on routes tests

--- server/src/test/kotlin/com/darkrockstudios/apps/hammer/projects/routes/ProjectsRoutesCreateProjectTest.kt
+++ server/src/test/kotlin/com/darkrockstudios/apps/hammer/projects/routes/ProjectsRoutesCreateProjectTest.kt
@@ -1,91 +1,20 @@
-package com.darkrockstudios.apps.hammer.projects
+package com.darkrockstudios.apps.hammer.projects.routes
 
-import com.darkrockstudios.apps.hammer.account.AccountsComponent
-import com.darkrockstudios.apps.hammer.account.AccountsRepository
-import com.darkrockstudios.apps.hammer.admin.AdminComponent
-import com.darkrockstudios.apps.hammer.admin.WhiteListRepository
 import com.darkrockstudios.apps.hammer.base.http.HEADER_SYNC_ID
-import com.darkrockstudios.apps.hammer.plugins.configureRouting
-import com.darkrockstudios.apps.hammer.plugins.configureSecurity
-import com.darkrockstudios.apps.hammer.plugins.configureSerialization
 import com.darkrockstudios.apps.hammer.project.InvalidSyncIdException
-import com.darkrockstudios.apps.hammer.project.ProjectRepository
 import com.darkrockstudios.apps.hammer.utilities.SResult
-import com.darkrockstudios.apps.hammer.utils.BaseTest
-import com.darkrockstudios.apps.hammer.utils.setupKtorTestKoin
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.isSuccess
-import io.ktor.server.application.Application
-import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
-import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.impl.annotations.MockK
-import kotlinx.serialization.json.Json
-import org.junit.Before
-import org.koin.dsl.module
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class ProjectsRoutesCreateProjectTest : BaseTest() {
-
-	@MockK(relaxed = true)
-	private lateinit var accountsRepository: AccountsRepository
-
-	@MockK(relaxed = true)
-	private lateinit var whiteListRepository: WhiteListRepository
-
-	@MockK(relaxed = true)
-	private lateinit var projectRepository: ProjectRepository
-
-	@MockK(relaxed = true)
-	private lateinit var projectsRepository: ProjectsRepository
-
-	@MockK(relaxed = true)
-	private lateinit var accountsComponent: AccountsComponent
-
-	@MockK(relaxed = true)
-	private lateinit var adminComponent: AdminComponent
-
-	@MockK(relaxed = true)
-	private lateinit var json: Json
-
-	private lateinit var testModule: org.koin.core.module.Module
-
-	private val BEARER_TOKEN = "token-test"
-
-	@Before
-	override fun setup() {
-		super.setup()
-
-		MockKAnnotations.init(this, relaxUnitFun = true)
-
-		testModule = module {
-			single { accountsRepository }
-			single { whiteListRepository }
-			single { projectRepository }
-			single { projectsRepository }
-			single { accountsComponent }
-			single { adminComponent }
-			single { json }
-		}
-	}
-
-	private fun ApplicationTestBuilder.defaultApplication(moreSetup: (Application.() -> Unit)? = null) {
-		application {
-			setupKtorTestKoin(this@ProjectsRoutesCreateProjectTest, testModule)
-
-			configureSerialization()
-			configureSecurity()
-			configureRouting()
-
-			if (moreSetup != null) moreSetup()
-		}
-	}
+class ProjectsRoutesCreateProjectTest : ProjectsRoutesBaseTest() {
 
 	@Test
 	fun `Projects - Create Project - Success`() = testApplication {
